4bad3e768bbba05c5415cab03ec2203b05f5454f
Nikesh Ashar <nikeshashar@gmail.com>
2014-06-26 13:43:42 +0100

    Add load from file option

--- directory.rb
+++ directory.rb
@@ -1,13 +1,10 @@
 require "date"
 @students = []
 def input_students
-	#creates an empty array
-	# students = []
 loop do
 	p "Please enter the names of the students"
 	p "To finish just hit return twice"
 	name = gets.gsub(/\n/, "")
-	#cohort = ""
 	break if name.empty?
 
 	loop do
@@ -20,13 +17,11 @@ loop do
 	hobby = gets.gsub(/\n/, "")
 	p "What country were you born in?"
 	origin = gets.gsub(/\n/, "")
-	#add the details hash to the array
 	@students << {:name => name, :cohort => Date.parse(@cohort).strftime(format="%B").to_sym, :hobby => hobby, :origin => origin}
 	p "Now we have #{@students.length} #{pluralise('student', @students.length)} with their details"
 	puts ""
-	#get another name from the user	
+	
 end
-#return array of students with hobbies
 @students
 end
 
@@ -44,20 +39,11 @@ def print_students_list
 	count = count + 1 
 	end
 end
-	
-	# students.each do |student|
-	# p "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?("A", "a") && student[:name].length < 12
-	# end
- 
 
 def print_footer
 	p "Overall we have #{@students.length} mighty #{pluralise('student', @students.length)}"
 	puts ""
 end
-
-# def sort_by_cohort(students)
-# 	students.sort_by!{|student| Date.parse(student[:cohort].to_s)}
-# end	
 
 def pluralise(word, count)
 	if count == 1
@@ -68,11 +54,11 @@ def pluralise(word, count)
 end
 
 def print_menu
-	# 1. print the menu and ask the user what to do
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
-    puts "9. Exit" # 9 because we'll be adding more items
+    puts "4. Load students from file"
+    puts "9. Exit"
 end
 
 def save_students
@@ -95,6 +81,16 @@ def show_students
 		puts ""
 	end	
 end
+
+def load_students
+	file=File.open("students.csv","r")
+	file.readlines.each do |line|
+		name, cohort, hobby, origin = line.chomp.split(',')
+		@students << {:name => name,:cohort => cohort.to_sym, :hobby => hobby, :origin => origin}
+	end
+	file.close
+end
+
 def process(selection)
 	case selection
 		when "1"
@@ -103,8 +99,10 @@ def process(selection)
 			show_students
 		when "3"
 			save_students
+		when "4"
+			load_students
 		when "9"
-			exit # this will cause the program to terminate
+			exit
 		else 
 			puts "Try again bud!"
 	end	
@@ -117,7 +115,6 @@ def interactive_menu
     puts ""
 	end
 end
-
 interactive_menu
 
 #nothing will happen until we do the last bit
@@ -131,3 +128,11 @@ interactive_menu
 # 	puts "you got no friends bro"
 # end
 
+	
+# students.each do |student|
+# p "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].start_with?("A", "a") && student[:name].length < 12
+# end
+ 
+# def sort_by_cohort(students)
+# 	students.sort_by!{|student| Date.parse(student[:cohort].to_s)}
+# end	
